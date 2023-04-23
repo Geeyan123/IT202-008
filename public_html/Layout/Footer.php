a4b0d234d6cf88988e491f5d55b3474a1eeb1d8f
Geeyan <gianc.r28@gmail.com>
2023-04-23 23:16:34 +0500

    layout added

---
+++ public_html/Layout/Footer.php
@@ -0,0 +1,3 @@
+<script type="text/javascript" src="<?php echo get_url('../Layout/mdb.min.js')?>"></script>
+    <!-- Custom scripts -->
+<script type="text/javascript"></script>
---
+++ public_html/Layout/Main.php
@@ -0,0 +1,53 @@
+<?php
+require_once(__DIR__ . "/../../lib/functions.php");
+//Note: this is to resolve cookie issues with port numbers
+$domain = $_SERVER["HTTP_HOST"];
+if (strpos($domain, ":")) {
+    $domain = explode(":", $domain)[0];
+}
+$localWorks = true; //some people have issues with localhost for the cookie params
+//if you're one of those people make this false
+
+//this is an extra condition added to "resolve" the localhost issue for the session cookie
+if (($localWorks && $domain == "localhost") || $domain != "localhost") {
+    session_set_cookie_params([
+        "lifetime" => 60 * 60,
+        "path" => "$BASE_PATH",
+        //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
+        "domain" => $domain,
+        "secure" => true,
+        "httponly" => true,
+        "samesite" => "lax"
+    ]);
+}
+session_start();
+
+
+?>
+
+
+<?php
+require(__DIR__ . "/Nav.php");
+?>
+
+<!-- include css and js files -->
+<script src="<?php echo get_url('helpers.js'); ?>"></script>
+<?php
+    require(__DIR__ . "/../Layout/Header.php");
+?>
+
+<?php
+    require(__DIR__ . "/../Layout/Footer.php");
+?>
+
+<?php
+require(__DIR__ . "/../../partials/flash.php");
+?>
+
+<?php
+
+if (is_logged_in(true)) {
+    //comment this out if you don't want to see the session variables
+    error_log("Session data: " . var_export($_SESSION, true));
+}
+?>
