c554755025a456b2b2de242a5b5dc15d75e595ef
Geeyaan123 <gianc.r28@gmail.com>
2023-05-05 00:00:14 +0500

    Payment information table added

---
+++ public_html/Project/payment.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+
+  <body>
+
+  <?php
+require(__DIR__ . "/../Layout/Main.php");
+?>
+
+
+<?php
+
+if (is_logged_in(true)) {
+    //comment this out if you don't want to see the session variables
+    error_log("Session data: " . var_export($_SESSION, true));
+}
+
+$db = getDB();
+$stmt = $db->prepare("select * FROM Cart AS c, Products AS p WHERE user_id=? AND c.product_id=p.id");
+$stmt->execute([get_user_id()]);
+$orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$sum = 0;
+foreach($orders as $order) { 
+    $sum = $sum + $order['desired_quantity'] * $order['unit_price'];
+}
+?>
+    <!-- Start your project here-->
+    <div class="container">
+    <section class="h-100 h-custom" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col">
+        <div class="card">
+          <div class="card-body p-4">
+
+
+
+              <div >
+
+                <div class="card bg-primary text-white rounded-3">
+                  <div class="card-body">
+                    <!-- <div class="d-flex justify-content-between align-items-center mb-4">
+                      <h5 class="mb-0">Card details</h5>
+                      <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp"
+                        class="img-fluid rounded-3" style="width: 45px;" alt="Avatar">
+                    </div> -->
+
+                    <p class="small mb-2">Card type</p>
+                     <div class="form-group">
+
+                        <select class="form-control" id="exampleFormControlSelect1">
+                          <option value="Cash">Cash</option>
+                          <option value="Visa">Visa</option>
+                          <option value="MasterCard">Master Card</option>
+                          <option value="Amex">Amex</option>
+
+                        </select>
+                      </div>
+
+                    <form class="mt-4">
+                      <div class="form-outline form-white mb-4">
+                        <input type="text" id="typeName" class="form-control form-control-lg" siez="17"
+                          placeholder="Cardholder's Name" />
+                        <label class="form-label" for="typeName">Cardholder's Name</label>
+                      </div>
+
+                      <div class="form-outline form-white mb-4">
+                        <input type="text" id="typeText" class="form-control form-control-lg" siez="17"
+                          placeholder="1234 5678 9012 3457" minlength="19" maxlength="19" />
+                        <label class="form-label" for="typeText">Card Number</label>
+                      </div>
+
+                      <div class="row mb-4">
+                        <div class="col-md-6">
+                          <div class="form-outline form-white">
+                            <input type="text" id="typeExp" class="form-control form-control-lg"
+                              placeholder="MM/YYYY" size="7" id="exp" minlength="7" maxlength="7" />
+                            <label class="form-label" for="typeExp">Expiration</label>
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-outline form-white">
+                            <input type="password" id="typeText" class="form-control form-control-lg"
+                              placeholder="&#9679;&#9679;&#9679;" size="1" minlength="3" maxlength="3" />
+                            <label class="form-label" for="typeText">Cvv</label>
+                          </div>
+                        </div>
+                      </div>
+
+                    </form>
+                   
+                    <hr class="my-4">
+
+                    <div class="d-flex justify-content-between">
+                      <p class="mb-2">Subtotal</p>
+                      <p class="mb-2"><?php echo $sum ?></p>
+                    </div>
+
+                    <div class="d-flex justify-content-between">
+                      <p class="mb-2">Shipping</p>
+                      <p class="mb-2">$20.00</p>
+                    </div>
+
+                    <div class="d-flex justify-content-between mb-4">
+                      <p class="mb-2">Total(Incl. taxes)</p>
+                      <p class="mb-2"><?php echo $sum + 20 ?></p>
+                    </div>
+
+            
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
+    <!-- End your project here-->
+
+
+  </body>
+</html>
+
+
+
